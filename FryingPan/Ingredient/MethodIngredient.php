a6ee03091e88fe74b4ff327b8441bf69a23a3d30
lingtalfi <lingtalfi@gmail.com>
2020-08-18 10:24:04 +0200

    update update ClassCooker->addMethod, add checkDuplicate option

--- FryingPan/Ingredient/MethodIngredient.php
+++ FryingPan/Ingredient/MethodIngredient.php
@@ -4,6 +4,8 @@
 namespace Ling\ClassCooker\FryingPan\Ingredient;
 
 
+use Ling\Bat\CommentTool;
+
 /**
  * The MethodIngredient class.
  *
@@ -24,27 +26,49 @@ class MethodIngredient extends BaseIngredient
     public function execute()
     {
         list($methodName, $options) = $this->valueInfo;
+        $addAsComment = $options['addAsComment'] ?? false;
         $cooker = $this->fryingPan->getCooker();
         $className = $cooker->getClassName();
 
+
+        $hasMethod = false;
         if (true === $cooker->hasMethod($methodName)) {
-            $this->fryingPan->sendToLog("The method \"$methodName\" is already found in class \"$className\".", 'skip');
-        } else {
+            $hasMethod = true;
+            if (false === $addAsComment) {
+                $this->fryingPan->sendToLog("The method \"$methodName\" is already found in class \"$className\".", 'skip');
+            } else {
+                $this->fryingPan->sendToLog("The method \"$methodName\" was found in class \"$className\", will try add it as comments.", 'skip');
+            }
+        }
+
+
+        if (false === $hasMethod ||
+            (true === $hasMethod && true === $addAsComment)
+        ) {
             if (array_key_exists('template', $options)) {
                 $template = $options['template'];
-                $this->fryingPan->sendToLog("Adding method \"$methodName\" to class \"$className\".", 'add');
+
+
+                $sAsComment = '';
+                if (true === $hasMethod) {
+                    $sAsComment = ' as comment';
+                    $template = CommentTool::comment($template) . PHP_EOL; // the php_eol here is crucial
+                }
+
+
+                $this->fryingPan->sendToLog("Adding method \"$methodName\" to class \"$className\"$sAsComment.", 'add');
 
                 unset($options['template']);
                 $options['throwEx'] = false;
+                $options['checkDuplicate'] = false;
                 $cooker->addMethod($methodName, $template, $options);
-
-
 
 
             } else {
                 $this->fryingPan->sendToLog("template option not found for the MethodIngredient.", 'error');
             }
         }
+
 
     }
 
